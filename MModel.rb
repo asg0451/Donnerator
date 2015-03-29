bc335963381ad6874dbb6be91b628357a01cc1e3
Miles Frankel <miles.frankel@gmail.com>
2015-03-29 00:53:46 -0400

    added replacement hash

--- MModel.rb
+++ MModel.rb
@@ -7,16 +7,24 @@ module MModel
 
   class MarkovModel
     def initialize
-      @markov_model = Hash.new { |h,k| h[k] = []}
 
+      @repl_words = {
+        you: "ya",
+        nothing: "nothin",
+        are: "be",
+        is: "be",
+        me: "i"
+      }
+
+      @markov_model = Hash.new { |h,k| h[k] = []} # hash of keys to lists
       isms = Donnerisms::Isms.new.isms
       isms.each do |i|
-	tokens = tokenize(i)
-	until tokens.empty?
-	  token = tokens.pop
-	  markov_state = tokens[-1] #[tokens[-2], tokens[-1]]
-	  @markov_model[markov_state] << token
-	end
+        tokens = tokenize(i)
+        until tokens.empty?
+          token = tokens.pop
+          markov_state = tokens[-1] #[tokens[-2], tokens[-1]]
+          @markov_model[markov_state] << token
+        end
       end
     end
 
@@ -26,14 +34,15 @@ module MModel
       tokens = tokenize(sentence)
       # binding.pry
       until sentence_complete?(tokens, min_length, max_length)
-	markov_state = tokens[-1]   # [tokens[-2], tokens[-1]]
-	tokens << @markov_model[markov_state].sample
+        markov_state = tokens[-1]   # [tokens[-2], tokens[-1]]
+        tokens << @markov_model[markov_state].sample
       end
+
       tokens.map do |w|
-	if w == "you"
-	  (rand(2) == 1) ? "ya" : w
-	else w end
+        r = @repl_words.fetch(w, w)  # default is original
+        (rand(2) == 1) ? r.to_s : w
       end . join(' ').strip
+
     end
 
     def tokenize(sentence)
@@ -43,7 +52,7 @@ module MModel
 
     def sentence_complete?(tokens, min_length, max_length)
       tokens.length >= max_length || tokens.length >= min_length &&
-	(tokens.last.nil? || tokens.last =~ /[\!\?\.]\z/)
+        (tokens.last.nil? || tokens.last =~ /[\!\?\.]\z/)
     end
 
   end
